5a2271d6c73319ee7ca408cb2913e72e66c0ea82
kjgwapo <100656396+kjgwapo@users.noreply.github.com>
2023-10-14 14:40:25 +0800

    updated UI

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,7 @@ import { Link, Switch, Route } from 'react-router-dom';
 import Dashboard from '../Dashboard';
 import Home from '../Home';
 
-import './custom-styles.css'; // Use your own CSS file
+import './styles.css'; 
 
 class Main extends Component {
   state = { activePage: '' };
@@ -21,8 +21,7 @@ class Main extends Component {
   }
 
   handleSearch = () => {
-    // Handle movie search using a movie API
-    // You can make an API request to search for movies based on user input
+    
   }
 
   render() {
@@ -40,11 +39,17 @@ class Main extends Component {
     };
 
     return (
+
+        
       <div style={backgroundStyle}>
-        <nav className="navbar navbar-dark bg-dark">
+          <div className="search-bar">
+          <input type="text" placeholder="Search for movies..." />
+          <button onClick={this.handleSearch}>Search</button>
+        </div>
+        <nav className="navbar navbar-dark bg-success">
           <div className="container">
             <Link to="/" className="navbar-brand" onClick={() => this.handleNavigationClick('home')}>
-              MovieHub
+              Animelist
             </Link>
             <Link to="/home" className={this.state.activePage === 'home' ? "nav-link active" : "nav-link"} style={{ ...navLinkStyle, marginRight: '10px' }} onClick={() => this.handleNavigationClick('home')}>
               Home
@@ -52,13 +57,11 @@ class Main extends Component {
             <Link to="/dashboard" className={this.state.activePage === 'dashboard' ? "nav-link active" : "nav-link"} style={{ ...navLinkStyle, marginRight: '10px' }} onClick={() => this.handleNavigationClick('dashboard')}>
               Dashboard
             </Link>
+            
           </div>
         </nav>
 
-        <div className="search-bar">
-          <input type="text" placeholder="Search for movies..." />
-          <button onClick={this.handleSearch}>Search</button>
-        </div>
+      
 
         <Switch>
           <Route path="/dashboard" component={Dashboard} />
